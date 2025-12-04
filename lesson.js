7f61ce5a64ed71842b10d93848b5a2c75af0ce76
Shoire <soiraabdusattarova@gmail.com>
2025-12-04 15:12:11 +0600

    first commit

---
+++ lesson.js
@@ -0,0 +1,44 @@
+//      условные операторы
+// 1 ==равно
+// 2 === строгое равенство
+// 3 > больше
+// 4 < меньше
+// 5 >= больше либо равно
+// 6 <= меньше либо равно
+// 7 !== не равно
+
+
+// esmasript
+// let a = 10
+// console.log(a);
+// a  += 5
+// console.log(a);
+// a  -= 5
+// console.log(a);
+
+
+//  let age = +prompt('сколько тебя лет?');
+//  if(age == 18){
+//     console.log("18");
+//  }else if(age == 20){
+//     console.log('20');
+//  }else if(age == 25){
+//     console.log('25');
+//  }else{
+//     console.log('sqcQW');
+    
+//  }
+
+
+let ffff = prompt('как тебя зовут?');
+  if(ffff === 'Asan' ){
+     console.log('Hello Asan!');
+  }else if( ffff === 'Usun'){
+    console.log('Hello Usun!');
+  }else if( ffff ==='Marat'){
+     console.log('Hello Marat!');
+  }else{
+    console.log('error');
+    
+  }
+
